3c7665accdbab1bcd3b95204eb912ef18e9b15be
MiMonMin <qminh293@gmail.com>
2017-07-31 21:56:58 +0700

    Add files via upload

---
+++ CustomerDEMO/src/Customer/servlet/EditProductServlet.java
@@ -0,0 +1,78 @@
+package Customer.servlet;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Customer.beans.Product;
+import Customer.utils.DBUtils;
+import Customer.utils.MyUtils;
+
+/**
+ * Servlet implementation class EditProductServlet
+ */
+@WebServlet(urlPatterns = { "/editProduct" })
+public class EditProductServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EditProductServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		  Connection conn = MyUtils.getStoredConnection(request);
+		  
+	      String code = (String) request.getParameter("code");
+	 
+	      Product product = null;
+	 
+	      String errorString = null;
+	 
+	      try {
+	          product = DBUtils.findProduct(conn, code);
+	      } catch (SQLException e) {
+	          e.printStackTrace();
+	          errorString = e.getMessage();
+	      }
+	 
+	      // Không có lỗi.
+	      // Sản phẩm không tồn tại để edit.
+	      // Redirect sang trang danh sách sản phẩm.
+	      if (errorString != null && product == null) {
+	          response.sendRedirect(request.getServletPath() + "/productList");
+	          return;
+	      }
+	 
+	      // Lưu thông tin vào request attribute trước khi forward sang views.
+	      request.setAttribute("errorString", errorString);
+	      request.setAttribute("product", product);
+	 
+	      RequestDispatcher dispatcher = request.getServletContext()
+	              .getRequestDispatcher("/WEB-INF/view/editProductView.jsp");
+	      dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request,response);
+	}
+
+}
